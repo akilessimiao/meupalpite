77d46f97459d1e98a79a314b9c751efda84b7e84
Akiles Simiao <46615264+akilessimiao@users.noreply.github.com>
2025-08-28 00:49:13 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+// Atualiza a data no rodapé
+document.addEventListener("DOMContentLoaded", () => {
+  const dataAtual = new Date().toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+  });
+  document.getElementById("data-atualizacao").innerHTML = `<em>Atualizado em: ${dataAtual}</em>`;
+});
